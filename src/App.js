a43a55d6ebce355707c03978f51b41fb069fc724
jcpcabanada <jcpcabanada@gmail.com>
2021-11-30 19:43:26 -0800

    pushin
    Took 25 minutes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,26 @@
 import React from 'react';
 import User from "./components/User";
 import FollowerList from "./components/FollowerList";
-// import axios from 'axios';
+import axios from 'axios';
 
 // https://api.github.com/users/jcpcabanada and https://api.github.com/users/jcpcabanada/followers
 
 class App extends React.Component {
     state = {
         currentUser: "jcpcabanada",
-        user: {
-            avatar_url: "https://avatars.githubusercontent.com/u/79226951?v=4",
-            html_url: "https://github.com/jcpcabanada",
-            login: "jcpcabanada",
-            name: "Jordan Chaz Cabanada",
-            public_repos: 33,
-            followers: 4
+        user: {},
+        followers: []
+    }
 
-        },
-        followers: [
-            {
-                login: "ryanghoward",
-                avatar_url: "https://avatars.githubusercontent.com/u/72590723?v=4",
-                html_url: "https://github.com/ryanghoward"
-            },
-            {
-                login: "ryanghoward",
-                avatar_url: "https://avatars.githubusercontent.com/u/72590723?v=4",
-            },
-            {
-                login: "ryanghoward",
-                avatar_url: "https://avatars.githubusercontent.com/u/72590723?v=4",
-                html_url: "https://github.com/ryanghoward"
-            }
-        ]
+    componentDidMount() {
+        axios
+            .get(`https://api.github.com/users/${this.state.currentUser}`)
+            .then(res => {
+                console.log(res.data)
+            })
+            .catch(err => {
+                console.error(err)
+            })
     }
 
     render() {
--- src/components/Follower.js
+++ src/components/Follower.js
@@ -4,12 +4,12 @@ import React from "react";
 class Follower extends React.Component {
 
     render() {
+        const {follower} = this.props
+        // console.log(follower)
         return (
-            <div>
             <div className="follower">
-                <img width="150" src="https://avatars.githubusercontent.com/u/72590723?v=4"/>
-                <a target="_blank" href='https://github.com/ryanghoward'><p>ryanghoward</p></a>
-            </div>
+                <img width="150" src={follower.avatar_url}/>
+                <a target="_blank" href={follower.html_url}><p>{follower.login}</p></a>
             </div>
         )
     }
