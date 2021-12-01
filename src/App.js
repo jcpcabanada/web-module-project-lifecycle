660f458a5bcb485f3974db652400d3d0ab8df812
jcpcabanada <jcpcabanada@gmail.com>
2021-11-30 16:14:17 -0800

    first? forgot to fork first time
    Took 7 minutes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
+// https://api.github.com/users/jcpcabanada and https://api.github.com/users/jcpcabanada/followers
+
 class App extends React.Component {
-  render() {
-    return(<div>
-      Github Card
-    </div>);
-  }
+
+
+    render() {
+        return (
+            <div className="app">
+                <div className="top">
+                    <h1>GITHUB INFO</h1>
+                    <form>
+                        <input/>
+                        <button>Search</button>
+                    </form>
+                </div>
+                <div className="card">
+                    <>Card</>
+                </div>
+                <div className="bottom">
+                    <h2>Followers: </h2>
+                </div>
+            </div>);
+    }
 }
 
 export default App;
