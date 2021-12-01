25c0a3bc7fa60c0c68050d2c875d039d2fafe8f4
jcpcabanada <jcpcabanada@gmail.com>
2021-11-30 16:43:19 -0800

    getting there
    Took 24 minutes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+// import axios from 'axios';
 
 // https://api.github.com/users/jcpcabanada and https://api.github.com/users/jcpcabanada/followers
 
 class App extends React.Component {
+    state = {
 
+    }
 
     render() {
         return (
@@ -11,15 +14,27 @@ class App extends React.Component {
                 <div className="top">
                     <h1>GITHUB INFO</h1>
                     <form>
-                        <input/>
+                        <input placeholder="Github Handle"/>
                         <button>Search</button>
                     </form>
                 </div>
                 <div className="card">
-                    <>Card</>
+                    <img width="300" src="https://avatars.githubusercontent.com/u/79226951?v=4" alt='profile pic' />
+                    <a target='_blank' href="https://github.com/jcpcabanada" ><h2>Jordan Chaz Cabanada</h2></a>
+                    <p>(jcpcabanada)</p>
+                    <p>Total Repos: 32</p>
+                    <p>Total Followers: 4</p>
+                    {/*<User/>*/}
                 </div>
                 <div className="bottom">
                     <h2>Followers: </h2>
+                    <img width="150" src="https://avatars.githubusercontent.com/u/72590723?v=4"/>
+                    <a target="_blank" href='https://github.com/ryanghoward'><p>ryanghoward</p></a>
+                    <img width="150" src="https://avatars.githubusercontent.com/u/72590723?v=4"/>
+                    <a target="_blank" href='https://github.com/ryanghoward'><p>ryanghoward</p></a>
+                    <img width="150" src="https://avatars.githubusercontent.com/u/72590723?v=4"/>
+                    <a target="_blank" href='https://github.com/ryanghoward'><p>ryanghoward</p></a>
+                    {/*<FollowerList/>*/}
                 </div>
             </div>);
     }
